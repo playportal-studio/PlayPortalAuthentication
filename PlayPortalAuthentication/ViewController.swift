231fed2ee8c2a7c17b705b80ac1398b003f36b16
Lincoln Fraley <lincoln@dynepic.com>
2018-11-07 15:20:00 -0500

    Initial Commit

---
+++ PlayPortalAuthentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayPortalAuthentication
+//
+//  Created by Lincoln Fraley on 11/7/18.
+//  Copyright © 2018 Lincoln Fraley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
